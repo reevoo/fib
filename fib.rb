2cb2795d5f48a904f4630a1ca7f72979f027656d
Ed Robinson <ed.robinson@reevoo.com>
2015-12-16 14:39:13 +0000

    add loader.io token

--- fib.rb
+++ fib.rb
@@ -5,3 +5,7 @@ f = ->(x){ x < 2 ? x : f[x-1] + f[x-2] }
 get '/fib/:num' do
   f[params['num'].to_i].to_s
 end
+
+get '/loaderio-dc5129eb9f484b75cda8ea5a231abe1a/' do
+  'loaderio-dc5129eb9f484b75cda8ea5a231abe1a'
+end
